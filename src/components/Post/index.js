dcb78f81335311c601a7920b9cd729523ff10ecc
MukhtarA <33712120+MukhtarA@users.noreply.github.com>
2021-01-06 16:39:21 +0600

    fix: add props to Post

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -3,33 +3,35 @@ import {View, Text, Image} from 'react-native';
 import styles from './styles.js';
 
 const Post = (props) => {
+  const post = props.post;
+
   return (
     <View style={styles.container}>
       {/* Image */}
       <Image
         style={styles.image}
         source={{
-          uri:
-            'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
+          uri: post.image,
         }}
       />
 
       {/* Bed & Bedroom */}
-      <Text style={styles.bedrooms}>1 bed 1 bedroom</Text>
+      <Text style={styles.bedrooms}>
+        {post.bed} bed {post.bedroom} bedroom
+      </Text>
 
       {/* Type & Description */}
       <Text style={styles.description} numberOfLines={2}>
-        Bright room in the heart of the city, Bright room in the heart of the
-        city, Bright room in the heart of the city
+        {post.type}. {post.title}
       </Text>
 
       {/* Old price & new price */}
       <Text style={styles.prices}>
-        <Text style={styles.oldPrice}>$30 </Text>
-        <Text style={styles.price}> $20</Text> / night
+        <Text style={styles.oldPrice}>${post.oldPrice} </Text>
+        <Text style={styles.price}> ${post.newPrice}</Text> / night
       </Text>
       {/* Total price */}
-      <Text style={styles.totalPrice}>$230 total</Text>
+      <Text style={styles.totalPrice}>${post.totalPrice} total</Text>
     </View>
   );
 };
